401d2abc3a63bcfc79ff5ccbc3f073be130716b5
dyv21 <denis@artkerr.com>
2024-10-08 16:22:20 +0300

    Add login form

---
+++ src/common/components/Login/Login.tsx
@@ -0,0 +1,8 @@
+import {Button} from "@/common/components/ui/button.tsx";
+
+export const Login = () => (
+    <div>
+      <a href="/" className="mr-2 text-black">Log in</a>
+      <Button variant="default">Start Yor Free Trial</Button>
+    </div>
+  )
