3f54d5dc142873a5f1ce5035406382d2ff9c7097
dyv21 <denis@artkerr.com>
2024-10-09 11:58:36 +0300

    Add PopUp form for Login & Register form

--- src/common/components/Login/Login.tsx
+++ src/common/components/Login/Login.tsx
@@ -1,8 +1,26 @@
-import {Button} from "@/common/components/ui/button.tsx";
+
+import {LoginPopUp} from "@/common/components/LoginPopUp/LoginPopUp.tsx";
+
+const registerProps = {
+  buttonName: 'Start Yor Free Trial',
+  dialogTitle:'Start your free trial today',
+  dialogDescription: 'Register now',
+  submitButtonName:'Register',
+  variant: 'default'
+}
+
+const loginProps = {
+  buttonName: 'Log in',
+  dialogTitle: 'Log in',
+  dialogDescription: 'New to Monkey$Money? Sign up today.',
+  submitButtonName: 'Log in',
+  variant: 'link'
+}
 
 export const Login = () => (
     <div>
-      <a href="/" className="mr-2 text-black">Log in</a>
-      <Button variant="default">Start Yor Free Trial</Button>
-    </div>
+      {/*<a href="/" className="mr-2 text-black text-sm" >Log in</a>*/}
+      <LoginPopUp dialogData={loginProps} />
+      <LoginPopUp dialogData={registerProps} className='link' />
+    </div >
   )
--- src/common/components/ui/button.tsx
+++ src/common/components/ui/button.tsx
@@ -16,8 +16,8 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        ghost: "hover:bg-accent hover:text-accent-foreground bg-background",
+        link: "text-primary underline-offset-4 hover:underline bg-background hover:bg-background hover:border-none hover:no-underline",
       },
       size: {
         default: "h-10 px-4 py-2",
